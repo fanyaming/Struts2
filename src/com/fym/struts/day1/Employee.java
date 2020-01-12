b18dd5dd5ba5dac49548a2b538bca19aa4516790
fan <643463659@qq.com>
2020-01-12 22:45:54 +0800

    Struts2学习

---
+++ src/com/fym/struts/day1/Employee.java
@@ -0,0 +1,15 @@
+package com.fym.struts.day1;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * Created by lenovo on 2020/1/12.
+ */
+@Setter@Getter@ToString
+public class Employee {
+    private Long id;
+    private String name;
+    private Integer age;
+}
